b5b2a4d5cda8021f7d16181d94eac4004182c6be
EmanueleVenditti95 <vendittiema@gmail.com>
2023-10-11 14:34:26 +0200

    Working on how to write function of event listener

--- app.js
+++ app.js
@@ -6,50 +6,41 @@ const pictures = [
     './img/04.webp',
     './img/05.webp'
 ]
-// console.log(pictures)
 
 // definire una variabile con il container del carosello
 const galleryDOMElement = document.getElementById('gallery')
-// console.log(gallery)
 
 // creare un ciclo per stampare tutte le immagini
 for (let i = 0; i < pictures.length; i++) {
     const currentPic = pictures[i]
-    console.log(currentPic)
 
 // inserire le immagini nell'html
     const htmlStringPic = `
      <div class="picture">
         <img src="${currentPic}">
      </div>`
-    console.log(htmlStringPic)
 
-    galleryDOMElement.innerHTML += htmlStringPic
+    galleryDOMElement.innerHTML += htmlStringPic    
 }
 
 // definire una variabile che seleziona tutti gli elementi della gallery
 const picturesDOMElement = document.querySelectorAll('.picture')
+console.log(picturesDOMElement)
 
-// definire una variabile per selezionare solo il primo elemento della gallery
-const firstPicDOMElement = picturesDOMElement[0]
-console.log(firstPicDOMElement)
+// definire una variabile per selezionare l'elemento della gallery corrente
+let currentIndex = 0
+let currentSlide = picturesDOMElement[currentIndex]
 
-// aggiungere la classe .active al primo elemento della gallery per renderlo visibile
-firstPicDOMElement.classList.add('active')
+// aggiungere la classe .active all'elemento della gallery corrente per renderlo visibile
+currentSlide.classList.add('active')
 
-// definire un ciclo per far avanzare le immagini nella gallery
-for (let i = 0; i < pictures.length; i++) {
-    let currentPic = picturesDOMElement[i]
-    document.getElementById('button-up').addEventListener("click", changePicture)
-    document.getElementById('button-down').addEventListener("click", changePicture)
+document.getElementById('button-down').addEventListener("click", changePicture)
 
-    function changePicture() {
-        if(document.getElementById('button-down')){
-            let currentPic = currentPic + [i]
-            currentPic.classList.add('active')
-        }
+function changePicture() {
+       
     }
-}
+console.log(currentSlide, currentIndex)
+
 // - SE clicco sulla freccia in giù:
 //     - l'immagine corrente dovrà diventare non visibile E l'immagine
 //       successiva dovrà diventare visibile
